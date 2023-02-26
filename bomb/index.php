25b63ec5bcd2fe9e393e9f4a03058ad291d4f934
ihara.mitsuki <mtkdonh@gmail.com>
2023-02-26 18:31:36 +0900

    first commit

---
+++ bomb/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bombTop</title>
+</head>
+<body>
+    <h2>個数！</h2>
+    <div> 
+        <a href="">9</a>
+    </div>
+    <div>
+        <a href="">16</a>
+    </div>
+    <div>
+        <a href="">25</a>
+    </div>
+</body>
+</html>
